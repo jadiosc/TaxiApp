d43a2f34577e515ac1d391b019d7566a6d18d99f
Augusto Valdez <jadiosc@gmail.com>
2019-11-03 23:21:47 -0600

    Se crea la base de OrderTaxi Widget
    Es la vista para Ordernar un Taxi

---
+++ lib/OrderTaxi.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// Activity for order a taxi
+class OrderTaxi extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+}
+
+class OrderTaxiState extends State<OrderTaxi> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
